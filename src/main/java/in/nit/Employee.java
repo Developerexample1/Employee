906659505c049125d6733c957dc905dd14a39485
Sonal Deo <Sonal Deo@LAPTOP-IAHCS7R7>
2020-02-23 17:33:06 +0530

    hashCode annd equals method

--- src/main/java/in/nit/Employee.java
+++ src/main/java/in/nit/Employee.java
@@ -2,6 +2,40 @@ package in.nit;
 
 public class Employee {
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((empId == null) ? 0 : empId.hashCode());
+		result = prime * result + ((empName == null) ? 0 : empName.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(empsal);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (empId == null) {
+			if (other.empId != null)
+				return false;
+		} else if (!empId.equals(other.empId))
+			return false;
+		if (empName == null) {
+			if (other.empName != null)
+				return false;
+		} else if (!empName.equals(other.empName))
+			return false;
+		if (Double.doubleToLongBits(empsal) != Double.doubleToLongBits(other.empsal))
+			return false;
+		return true;
+	}
 	private Integer empId;
 	private String empName;
 	private double empsal;
